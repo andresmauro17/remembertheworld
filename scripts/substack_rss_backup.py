c5ddb90c6f7da145f48673945609340f4c9e1eef
Andres Cuervo <andresmauro17@gmail.com>
2025-10-29 22:28:38 -0500

    🚧 Getting infor from rss

--- scripts/substack_rss_backup.py
+++ scripts/substack_rss_backup.py
@@ -1,7 +1,10 @@
 """Script to back up Substack RSS feeds."""
 
+import logging
 import os
+import ssl
 
+import feedparser
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,7 +14,23 @@ FEED_URL = os.environ.get("SUBSTACK_FEED_URL")
 
 def main() -> None:
     """main function"""
-    print(f"Fetching:{FEED_URL}")
+    logging.basicConfig(level=logging.INFO)
+
+    feedparser.USER_AGENT = "feedparser"
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+    logging.info(f"Fetching:{FEED_URL}")
+    feed: feedparser.util.FeedParserDict = feedparser.parse(FEED_URL)
+
+    if feed.bozo:
+        logging.warning("feedparser reported a problem parsing the feed.")
+        return None
+
+    for entry in feed.entries:
+        title = entry.title
+        link = entry.link
+        published = entry.published
+        logging.info(f"Title: {title}\nLink: {link}\nPublished: {published}\n")
 
 
 if __name__ == "__main__":
